0f3e49e873526976bccdac61b68df7a5f215b81a
rouich soufiane <rouichsoufiane@gmail.com>
2024-11-18 15:22:39 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// trigger the nav
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+const navClose = document.querySelector(".nav-close");
+const navOverlay = document.querySelector("#navOverlay");
+
+navToggle.addEventListener("click", () => {
+	nav.classList.add("nav-show");
+	navClose.classList.add("show");
+	navOverlay.classList.add("nav-overlay");
+});
+
+navClose.addEventListener("click", () => {
+	nav.classList.remove("nav-show");
+	navClose.classList.remove("show");
+	navOverlay.classList.remove("nav-overlay");
+});
